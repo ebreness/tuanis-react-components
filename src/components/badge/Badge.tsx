b344ce1aeb070d45fc77c810faf549ac4be0bf46
Erick Brenes Solano <dev@breness.com>
2024-08-13 20:20:52 -0600

    Removing Badge props to put the responsability of styles to the children component

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import {type CSSProperties, type ReactElement} from "react";
+import "./Badge.css";
 
 interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
     variant?: 'capsule' | 'dot';
-    bgColor: string;
-    labelColor?: string;
-    fontSize?: string;
+    color: string;
     borderRadius?: string,
     children: ReactElement;
 }
@@ -14,11 +13,9 @@ interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
  * Badge component to display labels with customizable styles.
  *
  * @param {'capsule' | 'dot'} [variant='capsule'] - The shape of the badge. 'capsule' for pill-like, 'dot' for a small circular badge.
- * @param {string} bgColor - The background color of the badge. When variant is 'dot' this parameter represents the dot color.
- * @param {string} [labelColor] - The color of the label text inside the badge. Defaults to the parent's text color.
+ * @param {string} color - The background color of the badge. When variant is 'dot' this parameter represents the dot color.
  * @param {ReactElement} children - The content of the badge, typically text.
- * @param {string} [fontSize] - The font size of the label text inside the badge. Defaults to 0.875rem.
- * @param {string} [borderRadius] - The border-radius of the badge when variant is 'capsule'. Defaults to 0.675rem.
+ * @param {string} [borderRadius] - The border-radius of the badge when variant is 'capsule'. Defaults to 1rem.
  * @param {React.HTMLAttributes<HTMLDivElement>} rest - Additional props passed to the badge element.
  * @returns {ReactElement} A styled badge element with the specified properties.
  * @constructor
@@ -30,18 +27,14 @@ interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
  */
 export const Badge = ({
                           variant = 'capsule',
-                          bgColor,
-                          labelColor,
+                          color,
                           children,
-                          fontSize,
                           borderRadius,
                           ...rest
                       }: BadgeProps): ReactElement => {
 
     const cssVariables: CSSProperties = {
-        "--trc-badge-bg-color": bgColor,
-        "--trc-badge-label-color": labelColor,
-        "--trc-badge-label-font-size": fontSize,
+        "--trc-badge-color": color,
         "--trc-badge-border-radius": borderRadius,
     } as CSSProperties;
 
